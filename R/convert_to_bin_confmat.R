bb360c8562002231e145df3ad83155e349ac0c0b
benmack <benjamin.mack@kit.edu>
2015-12-06 18:00:43 +0100

    +convert_to_bin_confmat

---
+++ R/convert_to_bin_confmat.R
@@ -0,0 +1,29 @@
+#'@title Convert a confusion matrix in a two-cass confusion matrix.
+#'@description ... 
+#'@param cm A confsion matrix with. Can by a table or a ConfusionMatrix 
+#'from caret.
+#'@param pos_class The name or column of the class which makes up the 
+#'first class of the new confusion matrix. 
+#'All other classes will be merged to one "other" class.   
+#'@param newnames Charater vector of length two with the classnames 
+#'of the returned two-class confusion matrix.
+#'@return A two-class table or ConfusionMatrix depending on the input. 
+#'@export
+convert_to_bin_confmat <- function(cm, pos_class, 
+                              newnames=c("Positive", "Negative")) {
+  return_ConfusionMatrix <- FALSE
+  if (class(cm) == "confusionMatrix"){
+    cm <- cm$table; 
+    return_ConfusionMatrix <- TRUE
+  }  
+  TP=cm[pos_class, pos_class]
+  FN=sum(cm[, pos_class]) - TP
+  FP=sum(cm[pos_class,]) - TP
+  TN=sum(cm) -(TP+FN+FP)
+  cm_bin <- as.table(matrix(c(TP, FN, FP, TP), 2, 2))
+  attr(cm_bin, "dimnames") <- list(Prediction=newnames,
+                                   Reference=newnames)
+  if (return_ConfusionMatrix)
+    cm_bin <- confusionMatrix(cm_bin)
+  return(cm_bin)
+}
